dc0cf3e63ce7990f5d891d06b42b756065ea6429
seenu1975 <seenu_gsr@yahoo.com>
2019-05-02 11:57:54 +0530

    1113

--- login.java
+++ login.java
@@ -2,6 +2,7 @@ public class login
 {
 public static void main(String args[])
 {
-System.out.println("hi, This is Seenu Rao");
+System.out.println("Hi, This is Seenu Rao");
+System.out.println("Hi, This is lokesh");
 }
 }
